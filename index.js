6222d20b7f25389dbe4d81317761389377443c1e
Marques <marquesrrs@gmail.com>
2026-06-17 22:58:06 -0300

    Initial styling

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+// TODO: 1. Create the server using express ✅
+// TODO: 2. Initialize this server ✅
+// TODO: 3. Create the home page using ejs ❌
+// TODO: 3.1 Create the common header ❌
+// TODO: 3.2 Create the common footer ❌
+// TODO: 3.3 Create the common style file
+// TODO: 3.4 Create the home page file ❌
+// TODO: 4. Create the post page ❌
+// TODO: 4.1 Add the post page style ❌
+// TODO: 5. Create the add post logic ❌
+// TODO: 5.1 Add the post creating page style ❌
+// TODO: 6. Connect home page to header and footer through .ejs ✅
 
 
 const app = express();
@@ -9,7 +21,11 @@ const hi = "Hi!";
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>{
-    res.render("../views/index.ejs", {greeting: hi});
+    res.render("../views/index.ejs");
+})
+
+app.get("/home", (req, res) =>{
+    res.render("../views/index.ejs");
 })
 
 app.listen(port, (req, resp) => {
